07d618426ed3321e2490efca8668e8c277614ab1
Milena Belianova <milena.belianova@gmail.com>
2022-08-11 13:12:49 +0400

    init: start react-tasks project

---
+++ react-components/src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { Header } from "./header/header";
+import { Footer } from "./footer/footer";
+import { Main } from "./main/main";
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        <Header showCartCount={true} cartCount={0} />
+        <Main />
+        <Footer />
+      </body>
+    </div>
+  );
+}
+
+export default App;
